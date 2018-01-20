c08c548a8356276713004418edf592f5b049b375
karn <karndubey94@gmail.com>
2018-01-20 19:22:19 +0530

    add new Property

--- network/lib/script.js
+++ network/lib/script.js
@@ -2,9 +2,16 @@
 
 */
 /**
-@param { org.acme.land.registry.Ownership_transfer } ownership
+@param { org.acme.land.registry.Add_property} p
 @transaction
 */
-function ownership_transfer(ownership){
- console.log("Ownership Changed"); 
+
+
+function add_property(p){
+	p.property.owner= p.newOwner;
+	return getAssetRegistry('org.acme.land.registry.Property')  
+		.then(function (assetRegistry)	{
+        	return assetRegistry.update(p.property);    
+		});
 }
+
